aa393bc6bc5bca66fc8954f54baffc0d6ce13c9a
Yours3lf <nekem28@msn.com>
2014-06-25 12:51:31 +0200

    added json example

--- Unnamed-Engine/Core_Systems/test.cpp
+++ Unnamed-Engine/Core_Systems/test.cpp
@@ -16,6 +16,56 @@
 #include "linear_allocator.h"
 #include "stack_allocator.h"
 
+#include "json/json.h"
+
+std::string json_str = "{ \"encoding\" : \"UTF-8\", \"plug-ins\" : [ \"python\", \"c++\", \"ruby\" ], \"indent\" : { \"length\" : 3, \"use_space\": true } }";
+
+void json_example()
+{
+  Json::Value root;   // will contains the root value after parsing.
+  Json::Reader reader;
+  bool parsingSuccessful = reader.parse( json_str, root );
+  if ( !parsingSuccessful )
+  {
+      // report to the user the failure and their locations in the document.
+      std::cout  << "Failed to parse configuration\n"
+                 << reader.getFormattedErrorMessages();
+      return;
+  }
+
+  // Get the value of the member of root named 'encoding', return 'UTF-8' if there is no
+  // such member.
+  std::string encoding = root.get("encoding", "UTF-8" ).asString();
+  // Get the value of the member of root named 'encoding', return a 'null' value if
+  // there is no such member.
+  const Json::Value plugins = root["plug-ins"];
+  for ( int index = 0; index < plugins.size(); ++index )  // Iterates over the sequence elements.
+     //loadPlugIn( plugins[index].asString() );
+     my::log << my::lock << plugins[index].asString() << my::endl << my::unlock;
+   
+  //setIndentLength( root["indent"].get("length", 3).asInt() );
+  //setIndentUseSpace( root["indent"].get("use_space", true).asBool() );
+
+  // ...
+  // At application shutdown to make the new configuration document:
+  // Since Json::Value has implicit constructor for all value types, it is not
+  // necessary to explicitly construct the Json::Value object:
+  //root["encoding"] = getCurrentEncoding();
+  //root["indent"]["length"] = getCurrentIndentLength();
+  //root["indent"]["use_space"] = getCurrentIndentUseSpace();
+
+  Json::StyledWriter writer;
+  // Make a new JSON document for the configuration. Preserve original comments.
+  std::string outputConfig = writer.write( root );
+
+  // You can also use streams.  This will put the contents of any JSON
+  // stream at a particular sub-value, if you'd like.
+  //std::cin >> root["subtree"];
+
+  // And you can write to a stream, using the StyledWriter automatically.
+  std::cout << root;
+}
+
 i32 main( i32 argc, i8** args )
 {
   crc32_init();
@@ -68,6 +118,8 @@ i32 main( i32 argc, i8** args )
   stack.free_to_marker( mark );
 
   stack.clear();
+
+  json_example();
 
   return 0;
 }
