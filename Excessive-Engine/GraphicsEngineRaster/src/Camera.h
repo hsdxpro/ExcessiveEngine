33972cc6825ea0eedc2c51e17d35269fec405511
Richárd Németh <hsdxpro@gmail.com>
2014-12-13 21:56:17 +0100

    Jesus again forget, camera parts

---
+++ Excessive-Engine/GraphicsEngineRaster/src/Camera.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "mymath\mm_common.h"
+#include "mymath\mymath.h"
+#include "..\..\Common\src\BasicTypes.h"
+#include "..\GraphicsEngine_Interface\interface\ICamera.h"
+
+class Camera : public graphics::ICamera {
+public:
+	void setFOV(float rad) override;
+	void setAspectRatio(float r) override;
+	void setNearPlane(float nP) override;
+	void setFarPlane(float fP) override;
+
+	void setPos(const mm::vec3& p) override;
+	void setTarget(const mm::vec3& p) override;
+	void setDir(const mm::vec3& p) override;
+
+	float getFOVRad() const override;
+	float getAspectRatio() const override;
+	float getNearPlane() const override;
+	float getFarPlane() const override;
+
+	mm::mat4 getViewMatrix() const override;
+	mm::mat4 getProjMatrix() const override;
+
+	mm::vec3 getDirFront() const override;
+	mm::vec3 getDirBack() const override;
+	mm::vec3 getDirUp() const override;
+	mm::vec3 getDirDown() const override;
+	mm::vec3 getDirRight() const override;
+	mm::vec3 getDirLeft() const override;
+
+	const mm::vec3& getPos() const override;
+	const mm::quat& getRot() const override;
+	const mm::vec3& getTarget() const override;
+
+	Camera(graphics::rProjOrtho proj, float nearPlane, float farPlane);
+	Camera(graphics::rProjPersp proj, float nearPlane, float farPlane);
+	Camera();
+protected:
+	void calcProjMatrix();
+protected:
+	// Pos, Target ( World space )
+	mm::vec3 pos;
+	mm::vec3 target;
+
+	// View params
+	float nearPlane;
+	float farPlane;
+
+	// Projection params ( TODO: little bit stupid to prepare camera class for every projection )
+	graphics::eProjType projType;
+	graphics::rProjOrtho projOrtho;
+	graphics::rProjPersp projPersp;
+	mm::mat4 proj;
+};
---
+++ Excessive-Engine/GraphicsEngine_Interface/interface/ICamera.h
@@ -0,0 +1,66 @@
+#pragma once
+#include "..\..\..\Externals\include\mymath\mymath.h"
+
+// namespace
+namespace graphics {
+
+////////////////////////////////////////////////////////////////////////////////
+///	Texture is a resource representing an image/bitmap.
+///
+////////////////////////////////////////////////////////////////////////////////
+
+enum class eProjType : u32 {
+	ORTHO,
+	PERSP,
+};
+
+struct rProjOrtho {
+	rProjOrtho(){}
+	rProjOrtho(float left, float right, float bottom, float top) :left(left), right(right), top(top), bottom(bottom){}
+
+	float left, right, top, bottom;
+};
+
+struct rProjPersp {
+	rProjPersp(){}
+	rProjPersp(float fovRad, float aspectRatio) :fovRad(fovRad), aspectRatio(aspectRatio){}
+
+	float fovRad;
+	float aspectRatio;
+};
+
+class ICamera
+{
+public:
+	virtual void setFOV(float rad) = 0;
+	virtual void setAspectRatio(float r) = 0;
+	virtual void setNearPlane(float nP) = 0;
+	virtual void setFarPlane(float fP) = 0;
+	
+	virtual void setPos(const mm::vec3& p) = 0;
+	virtual void setTarget(const mm::vec3& p) = 0;
+	virtual void setDir(const mm::vec3& p) = 0;
+	
+	virtual float getFOVRad() const = 0;
+	virtual float getAspectRatio() const = 0;
+	virtual float getNearPlane() const = 0;
+	virtual float getFarPlane() const = 0;
+	
+	virtual mm::mat4 getViewMatrix() const = 0;
+	virtual mm::mat4 getProjMatrix() const = 0;
+	
+	virtual mm::vec3 getDirFront() const = 0;
+	virtual mm::vec3 getDirBack() const = 0;
+	virtual mm::vec3 getDirUp() const = 0;
+	virtual mm::vec3 getDirDown() const = 0;
+	virtual mm::vec3 getDirRight() const = 0;
+	virtual mm::vec3 getDirLeft() const = 0;
+	
+	virtual const mm::vec3& getPos() const = 0;
+	virtual const mm::quat& getRot() const = 0;
+	virtual const mm::vec3& getTarget() const = 0;
+};
+
+
+
+} // namespace
