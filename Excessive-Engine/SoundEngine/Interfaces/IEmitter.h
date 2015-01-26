5fe6c7a901754922b1fef3b09c0ead97b21fa8f2
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2015-01-26 21:45:36 +0100

    Sound engine first interface...

---
+++ Excessive-Engine/SoundEngine/Interfaces/IEmitter.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <mymath/mymath.h>
+
+// namespace
+namespace sound {
+
+class ISoundData;
+
+class IEmitter {
+public:
+	virtual void setPos(const mm::vec3&) = 0;
+	virtual void setVel(const mm::vec3&) = 0;
+	
+	virtual mm::vec3 getPos() const = 0;
+	virtual mm::vec3 getVel() const = 0;
+	
+	virtual void setSoundData(ISoundData* data) = 0;
+	virtual ISoundData* getSoundData() const = 0;
+};
+
+} // namespace
---
+++ Excessive-Engine/SoundEngine/Interfaces/ISoundData.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../../Common/src/BasicTypes.h"
+
+// namespace
+namespace sound {
+
+class ISoundData {
+public:
+	virtual void release() = 0;
+
+	enum StoreMode : u8 {
+		STREAMED, /// Do not load sound file into memory, stream it from hard drive instead.
+		BUFFERED, /// Load complete file into memory.
+		AUTOMATIC, /// Decide automatically how to load sound.
+	}
+	
+	virtual bool load(const wchar_t* file_path, StoreMode mode = AUTOMATIC) = 0; 
+	
+};
+
+} // namespace
